e9f0e341711b671c6ae773c6f7ca6c917342d52d
Hasan Prayoga <hasandotprayoga@gmail.com>
2020-04-12 22:39:04 +0700

    first commit

---
+++ app/Http/Controllers/EscrowController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class EscrowController extends Controller
+{
+
+    protected $email = 'giokevin76@gmail.com';
+    protected $password = 'kevin123!';
+
+    protected $token;
+
+    protected $escrow = [];
+    protected $unpaid = [];
+    
+    public function index()
+    {
+        $this->login();
+        $this->unpaid();
+        $this->findEscrow();
+
+        return view('escrow', [
+            'data' => $this->escrow
+        ]);
+    }
+
+    protected function findEscrow()
+    {
+
+        $params = [
+            'data' => []
+        ];
+
+        foreach ($this->unpaid as $k => $v) {
+            $params['data'][] = [
+                'amount' => $v['gross_revenue'],
+                'referenceTimestamp' => $v['created_at']
+            ];
+        }
+
+        $response = Http::withHeaders([
+            'X-Requested-With' => 'XMLHttpRequest'
+        ])->post('https://gateway.yubiapi.net/v1/application/find/escrow', $params);
+
+        if ($response->ok()) {
+            $data = $response->json();
+            $escrow = $data['response']['results'];
+
+            $result = [];
+
+            foreach ($escrow as $k => $v) {
+                $key = array_search($v['amount'], array_column($this->unpaid, 'order_id'));
+                $v['orderId'] = $this->unpaid[$key]['order_id'];
+                $result[] = $v;
+            }
+
+            $this->escrow = $result;
+        }
+    }
+
+    protected function unpaid()
+    {
+
+        $token = $this->token;
+
+        $response = Http::withHeaders([
+            'authorization' => "Bearer $token"
+        ])->get('https://api.orderonline.id/submission', [
+            'limit' => 1000,
+            'sort_by' => 'created_at',
+            'sort' => 'desc',
+            'since' => '2020-02-20',
+            'until' => date('Y-m-d'),
+            'payment_status' => 'unpaid'
+        ]);
+
+        if ($response->ok()) {
+            $data = $response->json()['data'];
+            $this->unpaid = $data;
+        }
+    }
+
+    protected function login()
+    {
+        $response = Http::post('https://api.orderonline.id/auth', [
+            'email' => $this->email,
+            'password' => $this->password
+        ]);
+        
+        if ($response->ok()) {
+            $data = $response->json();
+            
+            if ($data['message'] == 'Login success') {
+                $this->token = $data['data']['access_token'];
+            }
+        }
+    }
+}
---
+++ resources/views/escrow.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Escrow</title>
+
+    </head>
+    <body>
+        <table border="1">
+            <tr>
+                <th>No.</th><th>OrderId</th><th>PaymentAt</th><th>Bank</th><th>Amount</th><th>Note</th>
+            </tr>
+            @forelse ($data as $key => $escrow)
+                <tr>
+                    <td>{{ $key + 1 }}</td>
+                    <td>{{ $escrow['orderId'] }}</td>
+                    <td>{{ $escrow['createdAt'] }}</td>
+                    <td>{{ $escrow['bank'] }}</td>
+                    <td>{{ $escrow['amount'] }}</td>
+                    <td>{{ $escrow['description'] }}</td>
+                </tr>
+            @empty
+                <div class="title m-b-md">
+                    <p>No users</p>
+                </div>
+            @endforelse
+        </table>
+    </body>
+</html>
